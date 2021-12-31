dde65cd80ce08d6282a30ad43344c859d11b2eb8
Emanuel Dumitru <emanuel.dumitru9@gmail.com>
2021-12-31 14:13:17 +0200

    Order summary view

--- core/views.py
+++ core/views.py
@@ -1,6 +1,9 @@
 from django.contrib import messages
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, View
 from django.shortcuts import redirect
 from django.utils import timezone
 from .models import Item, OrderItem, Order
@@ -28,11 +31,25 @@ class HomeView(ListView):
 #     }
 #     return render(request, "home.html", context)
 
+class OrderSummaryView(LoginRequiredMixin, View):
+    def get(self, *args, **kwargs):
+        try: 
+            order = Order.objects.get(user=self.request.user, ordered=False)
+            context = {
+                'object' : order
+            }
+            return render(self.request, "order_summary.html", context)
+        except ObjectDoesNotExist:
+            messages.error(self.request, "You do not have an active order")
+            return redirect("/")
+        
+
+
 class ItemDetailView(DetailView):
     model = Item
     template_name = "products.html"
 
-
+@login_required
 def add_to_cart(request, slug):
     item = get_object_or_404(Item, slug=slug)
     order_item, created = OrderItem.objects.get_or_create(
@@ -50,11 +67,11 @@ def add_to_cart(request, slug):
             order_item.quantity += 1
             order_item.save()
             messages.info(request, "This item quantity was updated.")
-            return redirect("core:products", slug=slug)
+            return redirect("core:order-summary")
         else:
             order.items.add(order_item)
             messages.info(request, "This item was added to your cart.")
-            return redirect("core:products", slug=slug)
+            return redirect("core:order-summary")
     
     else:
         ordered_date = timezone.now()
@@ -62,9 +79,9 @@ def add_to_cart(request, slug):
                                      ordered_date=ordered_date)
         order.items.add(order_item)
         messages.info(request, "This item was added to your cart.")
-        return redirect("core:products", slug=slug)
+        return redirect("core:order-summary")
 
-
+@login_required
 def remove_from_cart(request, slug):
     item = get_object_or_404(Item, slug=slug)
     order_qs = Order.objects.filter(
@@ -84,7 +101,42 @@ def remove_from_cart(request, slug):
             )[0]
             order.items.remove(order_item)
             messages.info(request, "This item was removed from your cart.")
+            return redirect("core:order-summary")
+        else:
+            # add a message saying the user doesn't have an order
+            messages.info(request, "This item was not in your cart.")
             return redirect("core:products", slug=slug)
+    else:
+        # add a message saying the user doesn't have an order
+        messages.info(request, "You do not have an active order.")
+        return redirect("core:products", slug=slug)
+    
+@login_required
+def remove_single_item_from_cart(request, slug):
+    item = get_object_or_404(Item, slug=slug)
+    order_qs = Order.objects.filter(
+        user=request.user,
+        ordered=False
+    )
+
+    if order_qs.exists():
+        order = order_qs[0]
+
+        #check if the order items is in the order
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item = OrderItem.objects.filter(
+                item=item,
+                user=request.user,
+                ordered=False
+            )[0]
+
+            if order_item.quantity > 1:
+                order_item.quantity -= 1
+                order_item.save()
+            else:
+                order.items.remove(order_item)
+            messages.info(request, "This item quantity was updated.")
+            return redirect("core:order-summary")
         else:
             # add a message saying the user doesn't have an order
             messages.info(request, "This item was not in your cart.")
