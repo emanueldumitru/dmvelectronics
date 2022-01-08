3ee249cdb22169be0c15b068fc8bdf56b25d77ad
Emanuel Dumitru <emanuel.dumitru9@gmail.com>
2022-01-08 19:24:34 +0200

    Worked on checkout part

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,7 @@ from .views import (
     HomeView,
     ItemDetailView,
     OrderSummaryView,
-    checkout, 
+    CheckoutView, 
     add_to_cart,
     remove_from_cart,
     remove_single_item_from_cart
@@ -13,7 +13,7 @@ app_name = 'core'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('checkout/', checkout, name='checkout'),
+    path('checkout/', CheckoutView.as_view(), name='checkout'),
     path('order-summary/', OrderSummaryView.as_view(), name='order-summary'),
     path('products/<slug>/', ItemDetailView.as_view(), name='products'),
     path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
